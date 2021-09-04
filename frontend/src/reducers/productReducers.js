4ca945d56702d2e7b8a4fdcb0d48a0e1a13aacec
E. Hannah <echannah631@gmail.com>
2021-09-04 16:00:54 -0700

    added redux implementation for specific product

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,6 +2,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_FETCHED,
   PRODUCT_LIST_FAIL,
+  PRODUCT_ITEM_REQUEST,
+  PRODUCT_ITEM_FETCHED,
+  PRODUCT_ITEM_FAIL,
 } from '../constants/productConstants';
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -11,6 +14,22 @@ export const productListReducer = (state = { products: [] }, action) => {
     case PRODUCT_LIST_FETCHED:
       return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productItemReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_ITEM_REQUEST:
+      return { loading: true, ...state };
+    case PRODUCT_ITEM_FETCHED:
+      return { loading: false, product: action.payload };
+    case PRODUCT_ITEM_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
