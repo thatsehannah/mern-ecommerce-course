bba8f704a892634d2d37ad93a41f3259bd801031
E. Hannah <echannah631@gmail.com>
2021-09-04 16:54:51 -0700

    added redux implementation for cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { addToCart } from '../actions/cartActions';
 
-const CartScreen = () => {
-  return (
-    <div>
-      Cart
-    </div>
-  )
-}
+const CartScreen = ({match, location, history}) => {
+  const productId = match.params.id
 
-export default CartScreen
+  //getting the qty from the query string (?qty=10)
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  }, [dispatch, productId, qty, match])
+
+  const { cartItems } = useSelector(state => state.cart)
+  console.log(cartItems)
+
+  return <div>Cart</div>;
+};
+
+export default CartScreen;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,13 +5,22 @@ import {
   productListReducer,
   productItemReducer,
 } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productItem: productItemReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage,
+  },
+};
 
 const middleware = [thunk];
 
