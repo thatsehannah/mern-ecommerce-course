32f6c11e06071eef783e758ef320569ec2265705
E. Hannah <echannah631@gmail.com>
2021-09-04 18:31:56 -0700

    added remove from cart action

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
-import {CART_ADD_ITEM} from '../constants/cartConstants'
+import axios from 'axios';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-  const {data} = await axios.get(`/api/products/${id}`)
+  const { data } = await axios.get(`/api/products/${id}`);
 
   dispatch({
     type: CART_ADD_ITEM,
@@ -12,9 +12,18 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       image: data.image,
       price: data.price,
       countInStock: data.countInStock,
-      qty
-    }
-  })
+      qty,
+    },
+  });
 
-  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-}
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  });
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -11,8 +11,7 @@ import {
 } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
-import Loader from '../components/Loader';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id;
@@ -29,7 +28,7 @@ const CartScreen = ({ match, location, history }) => {
   }, [dispatch, productId, qty, match]);
 
   const removeFromCartHandler = (id) => {
-    console.log('Remove');
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {
