8b7638636e72b6fb020f0bb82d367c1af01745bc
E. Hannah <echannah631@gmail.com>
2021-09-05 14:59:43 -0700

    able to register a new user, encrypted plain text password before saving new user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,40 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc      Register new user
+// @route     POST /api/users/
+// @access    Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExist = await User.findOne({ email: email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  // the create method is syntatic sugar for the '.save' method in the userSchema.pre() method in userModel
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
 // @desc      Get user profile
 // @route     GET /api/users/profile
 // @access    Private
@@ -43,4 +77,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
