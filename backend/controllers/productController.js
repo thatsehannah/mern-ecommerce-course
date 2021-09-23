d9284d3a840b6273e1975f280bf7178d93f5d73e
E. Hannah <echannah631@gmail.com>
2021-09-22 20:59:53 -0700

    uploading images

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -46,7 +46,7 @@ const createProduct = asyncHandler(async (req, res) => {
     name: 'Sample name',
     price: 0,
     user: req.user._id,
-    image: 'images/sample.jpg',
+    image: 'images/sample.jpeg',
     brand: 'Sample brand',
     category: 'Sample category',
     countInStock: 0,
@@ -68,13 +68,13 @@ const updateProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
   if (product) {
-    product.name = name
-    product.price = price
-    product.description = description
-    product.image = image
-    product.brand = brand
-    product.category = category
-    product.countInStock = countInStock
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
 
     const updatedProduct = await product.save();
     res.json(updatedProduct);
@@ -84,4 +84,10 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProductById, createProduct, updateProduct };
+export {
+  getProducts,
+  getProductById,
+  deleteProductById,
+  createProduct,
+  updateProduct,
+};
